f5a1749ee6b1455b448f658cbd386959c0836c45
smart <55507599@qq.com>
2022-04-02 00:33:51 +0800

    mysql bug

--- wed.go
+++ wed.go
@@ -17,7 +17,9 @@ import (
 	"gorm.io/gorm/schema"
 )
 
-var Mongodb *qmgo.Database
+var Mongo_db *qmgo.Database
+var Mysql_db *gorm.DB
+
 var config AllConfig
 
 func init() {
@@ -62,16 +64,14 @@ func Mongo() {
 	}
 	fmt.Println("mongodb 连接成功")
 	db := client.Database(config.Mongodb.DB)
-	Mongodb = db
+	Mongo_db = db
 	cli := db.Collection("user")
 	cli.EnsureIndexes(ctx, []string{"phoneNo"}, []string{}) //唯一索引
 }
 
-var MysqlDb *gorm.DB
-
 func Mysql() {
 	//dns := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&parseTime=True&loc=Local", config.Mysql.UserName, config.Mysql.Password, config.Mysql.Addr, config.Mysql.DB)
-	MysqlDb, mysqlError := gorm.Open(mysql.New(mysql.Config{
+	mysqldb, mysqlError := gorm.Open(mysql.New(mysql.Config{
 		DSN:               config.Mysql.Uri,
 		DefaultStringSize: 256, // string 类型字段的默认长度
 	}), &gorm.Config{
@@ -86,22 +86,19 @@ func Mysql() {
 			SingularTable: true,                     // 使用单数表名，启用该选项，此时，`User` 的表名应该是 `gee_user`
 		},
 	})
-
 	if mysqlError != nil {
 		fmt.Println("Myslq连接失败：", mysqlError)
 		os.Exit(1)
 	}
+	Mysql_db = mysqldb
 	fmt.Println("Myslq连接成功")
 	// 迁移数据表，在没有数据表结构变更时候，建议注释不执行
 	//_ = db.AutoMigrate(&User{}, &Article{}, &Category{}, Profile{}, Comment{})
-
-	sqlDB, _ := MysqlDb.DB()
+	sqlDB, _ := mysqldb.DB()
 	// SetMaxIdleCons 设置连接池中的最大闲置连接数。
 	sqlDB.SetMaxIdleConns(10)
-
 	// SetMaxOpenCons 设置数据库的最大连接数量。
 	sqlDB.SetMaxOpenConns(100)
-
 	// SetConnMaxLifetiment 设置连接的最大可复用时间。
 	sqlDB.SetConnMaxLifetime(10 * time.Second)
 }
